1f046acacd3d4fb1603a742281621c6209f9f39a
Samuel Mast <samuel.a.mast@gmail.com>
2018-05-08 11:09:28 -0600

    initial commit fix

---
+++ app/Http/Controllers/ListController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ListController extends Controller
+{
+    public function show()
+    {
+       $characters = [
+         'Step Brothers' 			=> '2008',
+         'Black Hawk Down'          => '2002',
+         'Dumb and Dumber'         	=> '1994',
+         'The Last of The Mohicans' => '1992',
+         'The Godfather'         	=> '1972'
+       ];
+
+       return view('welcome')->withCharacters($characters);
+    }
+}
